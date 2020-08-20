db4f23953705fa2e07dfc831c82fc0e8d1e1141a
Huafanfan <fanhua0425@gmail.com>
2020-08-20 12:15:37 +0800

    'update'

--- medium/KthSmallestElementinaBst0230.java
+++ medium/KthSmallestElementinaBst0230.java
@@ -1,5 +1,9 @@
 package medium;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+
 /**
  * @author Alex
  * @version 1.0
@@ -18,8 +22,38 @@ public class KthSmallestElementinaBst0230 {
             this.right = right;
         }
     }
+    int n;
+    int res;
+    public void inorder(TreeNode root, int k){
+        //剪枝
+        if(root == null || n > k) {
+            return;
+        }
+        inorder(root.left, k);
+        n++;
+        if(n == k) {
+            res = root.val;
+        }
+        inorder(root.right, k);
+    }
 
     public int kthSmallest(TreeNode root, int k) {
+        inorder(root, k);
+        return res;
+    }
 
+    public int kthSmallestIteration(TreeNode root, int k) {
+        LinkedList<TreeNode> stack = new LinkedList<TreeNode>();
+        while (true) {
+            while (root != null) {
+                stack.add(root);
+                root = root.left;
+            }
+            root = stack.removeLast();
+            if (--k == 0) {
+                return root.val;
+            }
+            root = root.right;
+        }
     }
 }
