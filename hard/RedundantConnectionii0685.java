669e8e453d2e46f30a96f790c184a09173e497ee
huafanfan <348324259@qq.com>
2020-09-17 11:33:19 +0800

    17/09/2020

---
+++ hard/RedundantConnectionii0685.java
@@ -0,0 +1,79 @@
+package hard;
+
+import sun.jvm.hotspot.oops.Array;
+
+import java.util.Arrays;
+import java.util.Collections;
+
+/**
+ * @author Alex
+ * @version 1.0
+ * @date 2020/9/17 10:25
+ */
+public class RedundantConnectionii0685 {
+    public static void main(String[] args) {
+        int[][] edges1 = new int[][]{{1,2},{1,3},{2,3}};
+        RedundantConnectionii0685 redundantConnectionii0685 = new RedundantConnectionii0685();
+        System.out.println(Collections.singletonList(redundantConnectionii0685.findRedundantDirectedConnection(edges1)));
+    }
+    public int[] findRedundantDirectedConnection(int[][] edges) {
+        int nodesCount = edges.length;
+        UnionFind uf = new UnionFind(nodesCount + 1);
+        int[] parent = new int[nodesCount + 1];
+        for (int i = 1; i <= nodesCount; ++i) {
+            parent[i] = i;
+        }
+        int conflict = -1;
+        int cycle = -1;
+        for (int i = 0; i < nodesCount; ++i) {
+            int[] edge = edges[i];
+            int node1 = edge[0], node2 = edge[1];
+            if (parent[node2] != node2) {
+                conflict = i;
+            } else {
+                parent[node2] = node1;
+                //并查集中分别找到 u 和 v 的祖先（即各自的连通分支中的根节点）
+                if (uf.find(node1) == uf.find(node2)) {
+                    cycle = i;
+                } else {
+                    uf.union(node1, node2);
+                }
+            }
+        }
+        if (conflict < 0) {
+            int[] redundant = {edges[cycle][0], edges[cycle][1]};
+            return redundant;
+        } else {
+            int[] conflictEdge = edges[conflict];
+            if (cycle >= 0) {
+                int[] redundant = {parent[conflictEdge[1]], conflictEdge[1]};
+                return redundant;
+            } else {
+                int[] redundant = {conflictEdge[0], conflictEdge[1]};
+                return redundant;
+            }
+        }
+    }
+
+    class UnionFind {
+        int[] ancestor;
+
+        public UnionFind(int n) {
+            ancestor = new int[n];
+            for (int i = 0; i < n; ++i) {
+                ancestor[i] = i;
+            }
+        }
+
+        public void union(int index1, int index2) {
+            ancestor[find(index1)] = find(index2);
+        }
+
+        public int find(int index) {
+            if (ancestor[index] != index) {
+                ancestor[index] = find(ancestor[index]);
+            }
+            return ancestor[index];
+        }
+    }
+}
