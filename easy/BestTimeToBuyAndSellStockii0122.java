cc47441a99decba5168df73c3aa956c490eba2d5
Huafanfan <fanhua0425@gmail.com>
2020-11-08 09:44:47 +0800

    08/11/2020

--- easy/BestTimeToBuyAndSellStockii0122.java
+++ easy/BestTimeToBuyAndSellStockii0122.java
@@ -1,4 +1,4 @@
-package com.huafanfan.easy;
+package easy;
 
 /**
  * @author Alex
@@ -90,5 +90,22 @@ public class BestTimeToBuyAndSellStockii0122 {
             }
         }
         return maxprofit;
+    }
+
+    public static int maxProfitDp(int[] prices) {
+        int n = prices.length;
+        //int[][] dp = new int[n][2];
+        //dp[0][0] = 0;
+        //dp[0][1] = -prices[0];
+        int dp0 = 0;
+        int dp1 = -prices[0];
+        for (int i=1; i<n; i++){
+            //dp[i][0] = Math.max(dp[i-1][0], dp[i-1][1] + prices[i]);
+            //dp[i][1] = Math.max(dp[i-1][1], dp[i-1][0] - prices[i]);
+            dp0 = Math.max(dp0, dp1 + prices[i]);
+            dp1 = Math.max(dp1, dp0 - prices[i]);
+        }
+        //return dp[n-1][0];
+        return dp0;
     }
 }
