31ca470df7a986dc2a659c7269d0ebdc8a06dcea
Huafanfan <fanhua0425@gmail.com>
2020-11-19 09:36:56 +0800

    19/11/2020

--- easy/MoveZeroes0283.java
+++ easy/MoveZeroes0283.java
@@ -1,4 +1,4 @@
-package com.huafanfan.easy;
+package easy;
 
 /**
  * @author Alex
@@ -19,5 +19,22 @@ public class MoveZeroes0283 {
                 l++;
             }
         }
+    }
+
+    public void moveZeroes2(int[] nums) {
+        int n = nums.length, left = 0, right = 0;
+        while (right < n) {
+            if (nums[right] != 0) {
+                swap(nums, left, right);
+                left++;
+            }
+            right++;
+        }
+    }
+
+    public void swap(int[] nums, int left, int right) {
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
     }
 }
