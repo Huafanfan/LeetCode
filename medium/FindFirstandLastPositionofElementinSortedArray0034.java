6a3509f192c9e5e7b473d275749adfe27c3abb22
Huafanfan <fanhua0425@gmail.com>
2020-12-22 11:43:36 +0800

    21/12/2020

---
+++ medium/FindFirstandLastPositionofElementinSortedArray0034.java
@@ -0,0 +1,31 @@
+package medium;
+
+/**
+ * @author Alex
+ * @version 1.0
+ * @date 2020/12/22 11:39
+ */
+public class FindFirstandLastPositionofElementinSortedArray0034 {
+    public int[] searchRange(int[] nums, int target) {
+        int leftIdx = binarySearch(nums, target, true);
+        int rightIdx = binarySearch(nums, target, false) - 1;
+        if (leftIdx <= rightIdx && rightIdx < nums.length && nums[leftIdx] == target && nums[rightIdx] == target) {
+            return new int[]{leftIdx, rightIdx};
+        }
+        return new int[]{-1, -1};
+    }
+
+    public int binarySearch(int[] nums, int target, boolean lower) {
+        int left = 0, right = nums.length - 1, ans = nums.length;
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] > target || (lower && nums[mid] >= target)) {
+                right = mid - 1;
+                ans = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+        return ans;
+    }
+}
