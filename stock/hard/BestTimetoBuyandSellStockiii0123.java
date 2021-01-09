a0bc4723948138a507bac93b03d9f270e86d8a3d
Huafanfan <fanhua0425@gmail.com>
2021-01-09 09:26:37 +0800

    09/01/2021

--- stock/hard/BestTimetoBuyandSellStockiii0123.java
+++ stock/hard/BestTimetoBuyandSellStockiii0123.java
@@ -153,35 +153,36 @@ public class BestTimetoBuyandSellStockiii0123 {
 
         //初始化状态
         //dp[0][0] = 0;
-        int dp0 = 0;
+        int sell0 = 0;
         //第一次买入
         //dp[0][1] = -prices[0];
-        int dp1 = -prices[0];
+        int buy1 = -prices[0];
         //第一次卖出
         //dp[0][2] = 0;
-        int dp2 = 0;
+        int sell1 = 0;
         //第二次买入
         //dp[0][3] = -prices[0];
-        int dp3 = -prices[0];
+        int buy2 = -prices[0];
         //第二次卖出
         //dp[0][4] = 0;
-        int dp4 = 0;
+        int sell2 = 0;
         for(int i=1;i<n;++i) {
             //dp[i][0]相当于初始状态，它只能从初始状态转换来
             //dp[i][0] = dp[i-1][0];
             //处理第一次买入、第一次卖出
             //dp[i][1] = Math.max(dp[i-1][1],dp[i-1][0]-prices[i]);
-            dp1 = Math.max(dp1, dp0 - prices[i]);
+            buy1 = Math.max(buy1, sell0 - prices[i]);
             //dp[i][2] = Math.max(dp[i-1][2],dp[i-1][1]+prices[i]);
-            dp2 = Math.max(dp2, dp1 + prices[i]);
+            sell1 = Math.max(sell1, buy1 + prices[i]);
             //处理第二次买入、第二次卖出
             //dp[i][3] = Math.max(dp[i-1][3],dp[i-1][2]-prices[i]);
-            dp3 = Math.max(dp3, dp2 - prices[i]);
+            buy2 = Math.max(buy2, sell1 - prices[i]);
             //dp[i][4] = Math.max(dp[i-1][4],dp[i-1][3]+prices[i]);
-            dp4 = Math.max(dp4, dp3 + prices[i]);
+            sell2 = Math.max(sell2, buy2 + prices[i]);
         }
         //返回最大值
         //return Math.max(Math.max( Math.max(dp[n-1][0],dp[n-1][1]),Math.max(dp[n-1][2],dp[n-1][3]) ),dp[n-1][4]);
-        return Math.max(0, Math.max(Math.max(dp1, dp2), Math.max(dp3, dp4)));
+        //return Math.max(0, Math.max(Math.max(dp1, dp2), Math.max(dp3, dp4)));
+        return sell2;
     }
 }
