8f38196fc83c41d440b1c4e3f544fda3317f61c5
Huafanfan <fanhua0425@gmail.com>
2021-01-18 14:12:29 +0800

    18/01/2021

---
+++ easy/CheckIfItIsaStraightLine1232.java
@@ -0,0 +1,25 @@
+package easy;
+
+/**
+ * @author Alex
+ * @version 1.0
+ * @date 2021/1/18 14:11
+ */
+public class CheckIfItIsaStraightLine1232 {
+    public boolean checkStraightLine(int[][] coordinates) {
+        int deltaX = coordinates[0][0], deltaY = coordinates[0][1];
+        int n = coordinates.length;
+        for (int i = 0; i < n; i++) {
+            coordinates[i][0] -= deltaX;
+            coordinates[i][1] -= deltaY;
+        }
+        int A = coordinates[1][1], B = -coordinates[1][0];
+        for (int i = 2; i < n; i++) {
+            int x = coordinates[i][0], y = coordinates[i][1];
+            if (A * x + B * y != 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
