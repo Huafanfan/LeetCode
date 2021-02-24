cde0a1a618f897005e38ac9dff3edfda7b58b014
Huafanfan <348324259@qq.com>
2021-02-24 10:12:31 +0800

    24/2/2021

---
+++ easy/FlippingAnImage0823.java
@@ -0,0 +1,22 @@
+package easy;
+
+public class FlippingAnImage0823 {
+    public int[][] flipAndInvertImage(int[][] A) {
+        int n = A.length;
+        for (int i = 0; i < n; i++) {
+            int left = 0, right = n - 1;
+            while (left < right) {
+                if (A[i][left] == A[i][right]) {
+                    A[i][left] ^= 1;
+                    A[i][right] ^= 1;
+                }
+                left++;
+                right--;
+            }
+            if (left == right) {
+                A[i][left] ^= 1;
+            }
+        }
+        return A;
+    }
+}
