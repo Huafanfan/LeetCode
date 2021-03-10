7b588a0cc8c4caded1e9e4025ca55ea9b4d1f327
Huafanfan <348324259@qq.com>
2021-03-10 13:58:31 +0800

    10/3/2021

---
+++ hard/BasicCalculator0224.java
@@ -0,0 +1,46 @@
+package hard;
+
+import java.util.Deque;
+import java.util.LinkedList;
+
+public class BasicCalculator0224 {
+    public int calculate(String s) {
+        Deque<Integer> ops = new LinkedList<Integer>();
+        ops.push(1);
+        int sign = 1;
+
+        int ret = 0;
+        int n = s.length();
+        int i = 0;
+        while (i < n) {
+            if (s.charAt(i) == ' ') {
+                i++;
+            } else if (s.charAt(i) == '+') {
+                sign = ops.peek();
+                i++;
+            } else if (s.charAt(i) == '-') {
+                sign = -ops.peek();
+                i++;
+            } else if (s.charAt(i) == '(') {
+                ops.push(sign);
+                i++;
+            } else if (s.charAt(i) == ')') {
+                ops.pop();
+                i++;
+            } else {
+                long num = 0;
+                while (i < n && Character.isDigit(s.charAt(i))) {
+                    num = num * 10 + s.charAt(i) - '0';
+                    i++;
+                }
+                ret += sign * num;
+            }
+        }
+        return ret;
+    }
+
+    public static void main(String[] args) {
+        BasicCalculator0224 basicCalculator0224 = new BasicCalculator0224();
+        System.out.println(basicCalculator0224.calculate("(1+(4+5+2)-3)+(6+8)"));
+    }
+}
