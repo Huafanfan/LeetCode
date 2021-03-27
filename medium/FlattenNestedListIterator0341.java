fc60a22f5573c84ca49d70f559fd946bfd62a9ef
Huafanfan <fanhua0425@gmail.com>
2021-03-27 13:31:24 +0800

    27/3/2021

--- medium/FlattenNestedListIterator0341.java
+++ medium/FlattenNestedListIterator0341.java
@@ -9,33 +9,33 @@ import java.util.List;
  * @version 1.0
  * @date 2021/3/23 08:28
  */
-public class FlattenNestedListIterator0341 implements Iterator<Integer> {
-    private List<Integer> vals;
-    private Iterator<Integer> cur;
+//public class FlattenNestedListIterator0341 implements Iterator<Integer> {
+//    private List<Integer> vals;
+//    private Iterator<Integer> cur;
 
-    public FlattenNestedListIterator0341(List<NestedInteger> nestedList) {
-        vals = new ArrayList<Integer>();
-        dfs(nestedList);
-        cur = vals.iterator();
-    }
-
-    @Override
-    public Integer next() {
-        return cur.next();
-    }
-
-    @Override
-    public boolean hasNext() {
-        return cur.hasNext();
-    }
-
-    private void dfs(List<NestedInteger> nestedList) {
-        for (NestedInteger nest : nestedList) {
-            if (nest.isInteger()) {
-                vals.add(nest.getInteger());
-            } else {
-                dfs(nest.getList());
-            }
-        }
-    }
-}
+    //public FlattenNestedListIterator0341(List<NestedInteger> nestedList) {
+    //    vals = new ArrayList<Integer>();
+    //    dfs(nestedList);
+    //    cur = vals.iterator();
+    //}
+    //
+    //@Override
+    //public Integer next() {
+    //    return cur.next();
+    //}
+    //
+    //@Override
+    //public boolean hasNext() {
+    //    return cur.hasNext();
+    //}
+    //
+    //private void dfs(List<NestedInteger> nestedList) {
+    //    for (NestedInteger nest : nestedList) {
+    //        if (nest.isInteger()) {
+    //            vals.add(nest.getInteger());
+    //        } else {
+    //            dfs(nest.getList());
+    //        }
+    //    }
+    //}
+//}
