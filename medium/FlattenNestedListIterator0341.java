aa93b1f3f270a7b29d9bbd2c7d0269ced0a059d1
Huafanfan <fanhua0425@gmail.com>
2021-03-27 12:44:45 +0800

    27/3/2021

---
+++ medium/FlattenNestedListIterator0341.java
@@ -0,0 +1,41 @@
+package medium;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * @author Alex
+ * @version 1.0
+ * @date 2021/3/23 08:28
+ */
+public class FlattenNestedListIterator0341 implements Iterator<Integer> {
+    private List<Integer> vals;
+    private Iterator<Integer> cur;
+
+    public FlattenNestedListIterator0341(List<NestedInteger> nestedList) {
+        vals = new ArrayList<Integer>();
+        dfs(nestedList);
+        cur = vals.iterator();
+    }
+
+    @Override
+    public Integer next() {
+        return cur.next();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return cur.hasNext();
+    }
+
+    private void dfs(List<NestedInteger> nestedList) {
+        for (NestedInteger nest : nestedList) {
+            if (nest.isInteger()) {
+                vals.add(nest.getInteger());
+            } else {
+                dfs(nest.getList());
+            }
+        }
+    }
+}
