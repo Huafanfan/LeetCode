cbefa2ca94eabb274c5968e64c0f9257ff4c9791
Huafanfan <348324259@qq.com>
2021-04-14 13:46:34 +0800

    14/4/2021

---
+++ medium/UglyNumberIi0264.java
@@ -0,0 +1,30 @@
+package medium;
+
+import java.util.Arrays;
+
+public class UglyNumberIi0264 {
+    public int nthUglyNumber(int n) {
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        int p2 = 1, p3 = 1, p5 = 1;
+        for (int i = 2; i <= n; i++) {
+            int num2 = dp[p2] * 2, num3 = dp[p3] * 3, num5 = dp[p5] * 5;
+            dp[i] = Math.min(Math.min(num2, num3), num5);
+            if (dp[i] == num2) {
+                p2++;
+            }
+            if (dp[i] == num3) {
+                p3++;
+            }
+            if (dp[i] == num5) {
+                p5++;
+            }
+        }
+        return dp[n];
+    }
+
+    public static void main(String[] args) {
+        UglyNumberIi0264 uglyNumberIi0264 = new UglyNumberIi0264();
+        System.out.println(uglyNumberIi0264.nthUglyNumber(10));
+    }
+}
