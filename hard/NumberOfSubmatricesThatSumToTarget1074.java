c53d94eba9317916f562084a0598616675a731fb
Huafanfan <fanhua0425@gmail.com>
2021-05-29 10:54:56 +0800

    29/5/2021

---
+++ hard/NumberOfSubmatricesThatSumToTarget1074.java
@@ -0,0 +1,40 @@
+package hard;
+
+import java.util.HashMap;
+
+/**
+ * @author Alex
+ * @version 1.0
+ * @date 2021/5/29 10:25
+ */
+public class NumberOfSubmatricesThatSumToTarget1074 {
+    public int numSubmatrixSumTarget(int[][] matrix, int target) {
+        int ans = 0;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        for (int i=0; i<m; i++){
+            int[] sum = new int[n];
+            for (int j=i; j<m; j++){
+                for (int k=0; k<n; k++){
+                    sum[k] += matrix[j][k];
+                }
+                ans += subarraySum(sum, target);
+            }
+        }
+        return ans;
+    }
+
+    public int subarraySum(int[] nums, int k) {
+        int count = 0, pre = 0;
+        HashMap< Integer, Integer > mp = new HashMap < > ();
+        mp.put(0, 1);
+        for (int num : nums) {
+            pre += num;
+            if (mp.containsKey(pre - k)) {
+                count += mp.get(pre - k);
+            }
+            mp.put(pre, mp.getOrDefault(pre, 0) + 1);
+        }
+        return count;
+    }
+}
