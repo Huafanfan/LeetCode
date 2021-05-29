0d822b5b593e3b7c4f4a4fedc4814414ba2b7fa1
Huafanfan <fanhua0425@gmail.com>
2021-05-29 15:59:59 +0800

    29/5/2021

--- hard/NumberOfSubmatricesThatSumToTarget1074.java
+++ hard/NumberOfSubmatricesThatSumToTarget1074.java
@@ -26,7 +26,7 @@ public class NumberOfSubmatricesThatSumToTarget1074 {
 
     public int subarraySum(int[] nums, int k) {
         int count = 0, pre = 0;
-        HashMap< Integer, Integer > mp = new HashMap < > ();
+        HashMap<Integer, Integer> mp = new HashMap<>();
         mp.put(0, 1);
         for (int num : nums) {
             pre += num;
@@ -36,5 +36,26 @@ public class NumberOfSubmatricesThatSumToTarget1074 {
             mp.put(pre, mp.getOrDefault(pre, 0) + 1);
         }
         return count;
+    }
+
+    public int numSubmatrixSumTarget2(int[][] mat, int t) {
+        int n = mat.length, m = mat[0].length;
+        int[][] sum = new int[n + 1][m + 1];
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                sum[i][j] = sum[i - 1][j] + sum[i][j - 1] - sum[i - 1][j - 1] + mat[i - 1][j - 1];
+            }
+        }
+        int ans = 0;
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                for (int p = 1; p <= i; p++) {
+                    for (int q = 1; q <= j; q++) {
+                        if (sum[i][j] - sum[p - 1][j] - sum[i][q - 1] + sum[p - 1][q - 1] == t) ans++;
+                    }
+                }
+            }
+        }
+        return ans;
     }
 }
