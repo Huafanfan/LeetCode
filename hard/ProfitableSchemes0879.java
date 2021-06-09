7dfdcae0e3044e63a1d710976f9981eeacb96e60
Huafanfan <348324259@qq.com>
2021-06-09 10:36:48 +0800

    9/6/2021

--- hard/ProfitableSchemes0879.java
+++ hard/ProfitableSchemes0879.java
@@ -2,6 +2,44 @@ package hard;
 
 public class ProfitableSchemes0879 {
     public int profitableSchemes(int n, int minProfit, int[] group, int[] profit) {
+        int len = group.length, MOD = (int)1e9 + 7;
+        int[][][] dp = new int[len + 1][n + 1][minProfit + 1];
+        dp[0][0][0] = 1;
+        for (int i = 1; i <= len; i++) {
+            int members = group[i - 1], earn = profit[i - 1];
+            for (int j = 0; j <= n; j++) {
+                for (int k = 0; k <= minProfit; k++) {
+                    if (j < members) {
+                        dp[i][j][k] = dp[i - 1][j][k];
+                    } else {
+                        dp[i][j][k] = (dp[i - 1][j][k] + dp[i - 1][j - members][Math.max(0, k - earn)]) % MOD;
+                        // max(0, k-earn)可以理解为 if(k <= earn) dp[i][j][k]=dp[i-1][j][k]+dp[i-1][j][0];
+                        // 如果earn自己就完成了盈利要求，那么前i-1个任务盈利为0即可 else dp[i][j][k] = dp[i-1][j][k]+dp[i-1][j][k-earn];
+                    }
+                }
+            }
+        }
+        int sum = 0;
+        for (int j = 0; j <= n; j++) {
+            sum = (sum + dp[len][j][minProfit]) % MOD;
+        }
+        return sum;
+    }
 
+    public int profitableSchemes2(int n, int minProfit, int[] group, int[] profit) {
+        int[][] dp = new int[n + 1][minProfit + 1];
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
+        }
+        int len = group.length, MOD = (int)1e9 + 7;
+        for (int i = 1; i <= len; i++) {
+            int members = group[i - 1], earn = profit[i - 1];
+            for (int j = n; j >= members; j--) {
+                for (int k = minProfit; k >= 0; k--) {
+                    dp[j][k] = (dp[j][k] + dp[j - members][Math.max(0, k - earn)]) % MOD;
+                }
+            }
+        }
+        return dp[n][minProfit];
     }
 }
