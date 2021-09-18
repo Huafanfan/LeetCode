f5e23cc9f45af9815dbd81979e0d7025c2b68ed3
Huafanfan <fanhua0425@gmail.com>
2021-09-18 10:34:01 +0800

    18/9/2021

---
+++ easy/NimGame0292.java
@@ -0,0 +1,12 @@
+package easy;
+
+/**
+ * @author Alex
+ * @version 1.0
+ * @date 2021/9/18 10:33
+ */
+public class NimGame0292 {
+    public boolean canWinNim(int n) {
+        return n%4 != 0;
+    }
+}
