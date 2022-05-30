f8ac0f2da2a5f116cfb9d1604ee2ac13cece7b1f
Huafanfan <fanhua0425@gmail.com>
2022-05-30 12:45:53 +0800

    30/5/2022

---
+++ easy/SumOfRootToLeafBinaryNumbers1022.java
@@ -0,0 +1,35 @@
+package easy;
+
+/**
+ * @author Alex
+ * @version 1.0
+ * @date 2022/5/30 12:19
+ */
+public class SumOfRootToLeafBinaryNumbers1022 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+    public int sumRootToLeaf(TreeNode root) {
+        return dfs(root, 0);
+    }
+
+    public int dfs(TreeNode root, int val) {
+        if (root == null) {
+            return 0;
+        }
+        val = (val << 1) | root.val;
+        if (root.left == null && root.right == null) {
+            return val;
+        }
+        return dfs(root.left, val) + dfs(root.right, val);
+    }
+}
