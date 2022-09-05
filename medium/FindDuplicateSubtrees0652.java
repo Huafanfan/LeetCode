4444b7f3ab1262a60442b8ad11398a9c4f549a37
Huafanfan <348324259@qq.com>
2022-09-05 22:07:57 +0800

    5/9/2022

---
+++ medium/FindDuplicateSubtrees0652.java
@@ -0,0 +1,50 @@
+package medium;
+
+import java.util.*;
+
+/**
+ * @author yifan.zhangyf
+ * @date 2022/9/5 9:52 PM
+ */
+public class FindDuplicateSubtrees0652 {
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+        TreeNode() {}
+        TreeNode(int val) { this.val = val; }
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+    Map<String, TreeNode> seen = new HashMap<String, TreeNode>();
+    Set<TreeNode> repeat = new HashSet<TreeNode>();
+
+    public List<TreeNode> findDuplicateSubtrees(TreeNode root) {
+        dfs(root);
+        return new ArrayList<TreeNode>(repeat);
+    }
+
+    public String dfs(TreeNode node) {
+        if (node == null) {
+            return "";
+        }
+        StringBuilder sb = new StringBuilder();
+        sb.append(node.val);
+        sb.append("(");
+        sb.append(dfs(node.left));
+        sb.append(")(");
+        sb.append(dfs(node.right));
+        sb.append(")");
+        String serial = sb.toString();
+        if (seen.containsKey(serial)) {
+            repeat.add(seen.get(serial));
+        } else {
+            seen.put(serial, node);
+        }
+        return serial;
+    }
+}
