e987018e2d7b5a63f042c75ea8c651a9f81d349f
Huafanfan <348324259@qq.com>
2022-09-24 16:53:09 +0800

    24/9/2022

---
+++ easy/DefuseTheBomb1653.java
@@ -0,0 +1,33 @@
+package easy;
+
+/**
+ * @author yifan.zhangyf
+ * @date 2022/9/24 12:00 PM
+ */
+public class DefuseTheBomb1653 {
+    public int[] decrypt(int[] code, int k) {
+        int n = code.length;
+        if (k == 0){
+            return new int[n];
+        }
+        int[] res = new int[n];
+        int[] newCode = new int[n * 2];
+        System.arraycopy(code, 0, newCode, 0, n);
+        System.arraycopy(code, 0, newCode, n, n);
+        code = newCode;
+        int l = k > 0 ? 1 : n + k;
+        int r = k > 0 ? k : n - 1;
+        int w = 0;
+        for (int i=l; i<=r; i++){
+            w += code[i];
+        }
+        for (int i=0; i<n; i++){
+            res[i] = w;
+            w -= code[l];
+            w += code[r + 1];
+            l++;
+            r++;
+        }
+        return res;
+    }
+}
